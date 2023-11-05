7cb05cb586d74b14313e3cc34fd929eebafff561
Estefania <estefania.goez@udea.edu.co>
2023-11-05 10:30:44 -0500

    Agregue nav bar a Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,22 @@
 const Dashboard = () => {
+  return (
+<nav className="navbar bg-body-tertiary ml-2" >
+  <div className="container">
+    <div className="d-flex align-items-center">
+      <img
+        src="src\images\DropoutWatchLogo.png"
+        alt="Logo"
+        width="80"
+        height="80"
+        className="d-inline-block "
+      />
+      <h1 className="display-6 mx-2">Dropout</h1>
+      <h1 className="mt-0 ">Watch</h1>
+    </div>
+  </div>
+</nav>
 
-    return (
-        <h1>Button</h1>
-    )
-
-}
+  );
+};
 
 export { Dashboard };
