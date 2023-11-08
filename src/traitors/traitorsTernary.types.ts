e3a7a23a834f2c2e13aca2507eb0ef6ccf404ff9
Jaime Muñoz <jaime.munozq@udea.edu.co>
2023-11-07 21:30:21 -0500

    add method to get traitors by a ternary of variables

---
+++ src/traitors/traitorsTernary.types.ts
@@ -0,0 +1,26 @@
+export interface TraitorsTernary {
+    ok:                          boolean;
+    totalTraitors:               number;
+    nom_unidad_acad_matriculado: string;
+    typeOfAdmissionsPorc:        ElementPorc[];
+    campusPorc:                  ElementPorc[];
+    programsPrc?:                 ElementPorc[];
+    versionPorc:                 ElementPorc[];
+    modalitiesPorc?:              ElementPorc[];
+    economicLevelPorc?:           ElementPorc[];
+    semesterCreditsRanges?:       Range[];
+    initialCreditsRanges:        Range[];
+    failedCreditRanges:          Range[];
+    CancelledCreditRanges:       Range[];
+}
+
+export interface Range {
+    min:      number;
+    max:      number;
+    traitors: number;
+}
+
+export interface ElementPorc {
+    category: string;
+    traitors: number;
+}
