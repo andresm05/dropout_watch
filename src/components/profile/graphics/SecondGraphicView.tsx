5f45054fd00cb073a3a4dbf684043ca1231529b6
Estefania <estefania.goez@udea.edu.co>
2023-11-21 12:54:13 -0500

    Deshice el cambio del push anterior a este

--- src/components/profile/graphics/SecondGraphicView.tsx
+++ src/components/profile/graphics/SecondGraphicView.tsx
@@ -56,36 +56,38 @@ export const SecondGraphicView = ({ traitors }: GraphicViewProps) => {
   return (
     <div className="card text-center border border-light">
       <div className="card-body">
-        <h5 className="card-title text-primary">Estadísticas adicionales</h5>
-        <div className="d-flex flex-wrap justify-content-center m-5">
-            {existField(traitors?.modalitiesPorc) && (
-              <div className="col-lg-6 mb-4">
-                <ReactECharts option={chartModalities} />
-              </div>
-            )}
-            {existField(traitors?.programsPrc) && (
-              <div className="col-lg-6 mb-4">
-                <ReactECharts option={chartPrograms} />
-              </div>
-            )}
-            {existField(traitors?.economicLevelPorc) && (
-              <div className="col-lg-6 mb-4">
-                <ReactECharts option={chartEconomicLevel} />
-              </div>
-            )}
-            {existField(traitors?.semesterCreditsRanges) && (
-              <div className="col-lg-6 mb-4">
-                <ReactECharts option={chartTotalCredits} />
-              </div>
-            )}
-            <div className="col-lg-6 mb-4">
-              <ReactECharts option={chartFailedCredits} />
-            </div>
-            <div className="col-lg-6 mb-4">
-              <ReactECharts option={chartCanceledCredits} />
-            </div>
+        <h5 className="card-title text-primary">Estadisticas adicionales</h5>
+        <div className="container">
+      <div className="row">
+        {existField(traitors?.modalitiesPorc) && (
+          <div className="col-lg-6 mb-4">
+            <ReactECharts option={chartModalities} />
           </div>
+        )}
+        {existField(traitors?.programsPrc) && (
+          <div className="col-lg-6 mb-4">
+            <ReactECharts option={chartPrograms} />
+          </div>
+        )}
+        {existField(traitors?.economicLevelPorc) && (
+          <div className="col-lg-6 mb-4">
+            <ReactECharts option={chartEconomicLevel} />
+          </div>
+        )}
+        {existField(traitors?.semesterCreditsRanges) && (
+          <div className="col-lg-6 mb-4">
+            <ReactECharts option={chartTotalCredits} />
+          </div>
+        )}
+        <div className="col-lg-6 mb-4">
+          <ReactECharts option={chartFailedCredits} />
         </div>
+        <div className="col-lg-6 mb-4">
+          <ReactECharts option={chartCanceledCredits} />
+        </div>
+      </div>
+    </div>
+      </div>
     </div>
   );
 };
